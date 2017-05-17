89972c32b2615bf2c14c9ffd6257e6662400d4b1
alen <alen@touzi.org>
2017-05-17 17:05:33 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ sshpass -p $DEPLOY_SSH_PASS ssh -o StrictHostKeyChecking=no $DEPLOY_SSH_URL "kil
 sshpass -p $DEPLOY_SSH_PASS ssh -o StrictHostKeyChecking=no $DEPLOY_SSH_URL "[ -f nanoweb ] && rm nanoweb "
 sshpass -p $DEPLOY_SSH_PASS scp -o stricthostkeychecking=no nanoweb ${DEPLOY_SSH_URL}:/home/ubuntu
 #sshpass -p $DEPLOY_SSH_PASS ssh -o StrictHostKeyChecking=no $DEPLOY_SSH_URL "NANOWEB_STATUS=`ps -A | grep nanoweb | wc -l` && [ $NANOWEB_STATUS -eq 1 ] && killall nanoweb && nohup ./nanoweb&"
-sshpass -p $DEPLOY_SSH_PASS ssh -o StrictHostKeyChecking=no $DEPLOY_SSH_URL "killall nanoweb ; nohup ./nanoweb&"
+sshpass -p $DEPLOY_SSH_PASS ssh -o StrictHostKeyChecking=no $DEPLOY_SSH_URL "killall nanoweb ; chmod a+x nanoweb ; nohup ./nanoweb&"
 
 exit 0;
